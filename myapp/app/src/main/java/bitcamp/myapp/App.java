e15cd1e5cde56d83b58d52d835b409ae22016168
shin <shin2012649@gmail.com>
2023-05-26 17:00:27 +0900

    update

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -5,19 +5,22 @@ public class App {
   public static void main(String[] args) {
     System.out.println("나의 목록 관리 시스템");
     System.out.println("==========================");
-    
-    System.out.println("번호: ");
-    System.out.println(100);
 
-    System.out.printf("이름: %s", "홍길동");
-    System.out.println();
+    int no = 100;
+    String name =  "홍길동";
+    int age = 20;
+    boolean working = true;
+    char gender = 'M';
+    float leftEye = 1.5f;
+    float rightEye = 1.0f;
 
-    System.out.println("나이: " +20);
+    System.out.printf("번호: %d\n ", no);
+    System.out.printf("이름: %s\n", name);
+    System.out.printf("나이: " +age);
+    System.out.printf("재직자: %b\n", working);
 
-    System.out.printf("재직자: %b\n", true);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n",gender);
 
-    System.out.printf("성별(남자(M), 여자(W)): %c \n",'M');
-
-    System.out.printf("좌우시력: %f,%f\n", 1.5f, 1.0f);
+    System.out.printf("좌우시력: %f,%f\n",leftEye,rightEye);
   }
 }
