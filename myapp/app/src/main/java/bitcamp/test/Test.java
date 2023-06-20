cacab7981733dbe9eb2a2c870956189819ecf1f4
shin2012649 <shin2012649@gmail.com>
2023-06-20 16:34:09 +0900

    update

--- myapp/app/src/main/java/bitcamp/test/Test.java
+++ myapp/app/src/main/java/bitcamp/test/Test.java
@@ -1,14 +1,15 @@
 package bitcamp.test;
 
 public class Test {
-  
   public static void main(String[] args) {
-    Calculator c  = new Calculator2();
-    
-//    Calculator2 c2 = (Calculator2) c; 뿡치지마라 
-    
-    System.out.println(((Calculator2)c).minus(100,200));
-    System.out.println(((Calculator)c).plus(100,200)); 
+    Student s = new Student();
+
+    s.school = "비트대학교";
+    s.major = "컴공";
+    s.working = false;
+
+    s.name = "홍길동";
+    s.email = "hong@test.com";
+    s.password = "1111";
   }
 }
-// 어떤 자바 클래스도 object의 자손이라 볼 수 있는거지 
