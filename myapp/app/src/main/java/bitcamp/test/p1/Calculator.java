f774dff9cd4e176e43fd75a76622a64e571d6946
shin2012649 <shin2012649@gmail.com>
2023-06-22 17:34:09 +0900

    update

---
+++ myapp/app/src/main/java/bitcamp/test/p1/Calculator.java
@@ -0,0 +1,14 @@
+package bitcamp.test.p1;
+
+public class Calculator {
+
+  public int result;
+
+  public void plus(int value) {
+    this.result += value;
+  }
+
+  public void minus(int value) {
+    this.result -= value;
+  }
+}
