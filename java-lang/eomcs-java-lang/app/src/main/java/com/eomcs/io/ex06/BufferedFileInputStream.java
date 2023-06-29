624240363f9bcceddf6d16e56de2ea1041416f84
shin2012649 <shin2012649@gmail.com>
2023-06-29 16:50:52 +0900

    update

--- java-lang/eomcs-java-lang/app/src/main/java/com/eomcs/io/ex06/BufferedFileInputStream.java
+++ java-lang/eomcs-java-lang/app/src/main/java/com/eomcs/io/ex06/BufferedFileInputStream.java
@@ -20,7 +20,7 @@ public class BufferedFileInputStream extends FileInputStream {
   // 상속 받은 메서드를 재정의 한다. 
   @Override
   public int read() throws IOException {
-    if (cursor == size) { // 바이트 배열에 저장되어 있는 데이터를 모두 읽었다면,
+    if (size == -1 || cursor == size) { // 바이트 배열에 저장되어 있는 데이터를 모두 읽었다면,
       if ((size = super.read(buf)) == -1) { // 다시 파일에서 바이트 배열로 데이터를 왕창 읽어 온다. 
         return -1;
       }
--- java-lang/eomcs-java-lang/app/src/main/java/com/eomcs/io/ex06/Exam0130.java
+++ java-lang/eomcs-java-lang/app/src/main/java/com/eomcs/io/ex06/Exam0130.java
@@ -4,7 +4,7 @@ package com.eomcs.io.ex06;
 public class Exam0130 {
 
   public static void main(String[] args) throws Exception {
-    BufferedFileInputStream in = new BufferedFileInputStream("temp/jls11.pdf");
+    BufferedFileInputStream in = new BufferedFileInputStream("temp/jls17.pdf");
 
     int b;
 
@@ -33,3 +33,10 @@ public class Exam0130 {
   }
 
 }
+
+//6295
+//5111425
+
+//26
+//5111425
+
