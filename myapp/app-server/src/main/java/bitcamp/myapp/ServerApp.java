328cef9e8b69255dd08688e9c5d27b0ee5bf4e3d
shin2012649 <shin2012649@gmail.com>
2023-07-14 19:21:17 +0900

    update

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -12,6 +12,8 @@ import bitcamp.myapp.dao.BoardListDao;
 import bitcamp.myapp.dao.MemberListDao;
 import bitcamp.net.RequestEntity;
 import bitcamp.net.ResponseEntity;
+import bitcamp.util.ManagedThread;
+import bitcamp.util.ThreadPool;
 
 public class ServerApp {
 
@@ -19,6 +21,9 @@ public class ServerApp {
   ServerSocket serverSocket;
 
   HashMap<String,Object> daoMap = new HashMap<>();
+
+  // 스레드를 리턴해 줄 스레드풀 준비
+  ThreadPool threadPool = new ThreadPool();
 
   public ServerApp(int port) throws Exception {
     this.port = port;
@@ -44,24 +49,15 @@ public class ServerApp {
   }
 
   public void execute() throws Exception {
-    class RequestAgentThread extends Thread {
-      Socket socket;
-      public RequestAgentThread(Socket socket) {
-        this.socket = socket;
-      }
-      @Override
-      public void run() {
-        processRequest(socket);
-      }
-    }
-
     System.out.println("[MyList 서버 애플리케이션]");
 
     this.serverSocket = new ServerSocket(port);
     System.out.println("서버 실행 중...");
 
     while (true) {
-      new RequestAgentThread(serverSocket.accept()).start();
+      Socket socket = serverSocket.accept();
+      ManagedThread t = threadPool.getResource();
+      t.setJob(() ->processRequest(socket));
     }
   }
 
@@ -93,6 +89,10 @@ public class ServerApp {
       System.out.printf("%s:%s 클라이언트가 접속했음!\n",
           socketAddress.getHostString(),
           socketAddress.getPort());
+
+      // 스레드풀이 새 스레드를 만드는 것을 테스트하기 위함.
+      // => 스레드풀에 스레드가 없을 때 새 스레드를 만들 것이다.
+      Thread.sleep(10000);
 
       // 클라이언트 요청을 반복해서 처리하지 않는다.
       // => 접속 -> 요청 -> 실행 -> 응답 -> 연결 끊기
@@ -141,7 +141,6 @@ public class ServerApp {
       System.out.println(e.getMessage());
     }
   }
-
 }
 
 
