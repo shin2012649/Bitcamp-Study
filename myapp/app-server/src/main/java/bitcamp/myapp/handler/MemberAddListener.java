d1995fef80836964fde136ade6cbdd9f5b2a3336
shin2012649 <shin2012649@gmail.com>
2023-08-02 17:50:42 +0900

    Update

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberAddListener.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberAddListener.java
@@ -1,14 +1,16 @@
 package bitcamp.myapp.handler;
 
-import java.io.IOException;
+import java.io.PrintWriter;
 import org.apache.ibatis.session.SqlSessionFactory;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
-import bitcamp.util.BreadcrumbPrompt;
 import bitcamp.util.Component;
+import bitcamp.util.HttpServletRequest;
+import bitcamp.util.HttpServletResponse;
+import bitcamp.util.Servlet;
 
 @Component("/member/add")
-public class MemberAddListener implements MemberActionListener {
+public class MemberAddListener implements Servlet {
 
   MemberDao memberDao;
   SqlSessionFactory sqlSessionFactory;
@@ -19,20 +21,38 @@ public class MemberAddListener implements MemberActionListener {
   }
 
   @Override
-  public void service(BreadcrumbPrompt prompt) throws IOException {
+  public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
     Member m = new Member();
-    m.setName(prompt.inputString("이름? "));
-    m.setEmail(prompt.inputString("이메일? "));
-    m.setPassword(prompt.inputString("암호? "));
-    m.setGender(MemberActionListener.inputGender((char)0, prompt));
+    m.setName(request.getParameter("name"));
+    m.setEmail(request.getParameter("email"));
+    m.setPassword(request.getParameter("password"));
+    m.setGender(request.getParameter("gender").charAt(0));
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset='UTF-8'>");
+    out.println("<meta http-equiv='refresh' content='1;url=/member/list'>");
+    out.println("<title>회원</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>회원 등록</h1>");
 
     try {
       memberDao.insert(m);
       sqlSessionFactory.openSession(false).commit();
+      out.println("<p>등록 성공입니다!</p>");
 
     } catch (Exception e) {
       sqlSessionFactory.openSession(false).rollback();
-      throw new RuntimeException(e);
+      out.println("<p>등록 실패입니다!</p>");
+      e.printStackTrace();
     }
+
+    out.println("</body>");
+    out.println("</html>");
   }
 }
