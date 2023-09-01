96c40db5ca27ae4dc27865944916428052ca9962
shin2012649 <shin2012649@gmail.com>
2023-09-01 19:01:39 +0900

    update

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/LoginController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/LoginController.java
@@ -11,38 +11,37 @@ import javax.servlet.http.HttpServletResponse;
 @Component("/auth/login")
 public class LoginController implements PageController {
 
-  MemberDao memberDao;
+    MemberDao memberDao;
 
-  public LoginController(MemberDao memberDao) {
-    this.memberDao = memberDao;
-  }
-
-  @Override
-  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    if (request.getMethod().equals("GET")) {
-      return "/WEB-INF/jsp/auth/form.jsp";
+    public LoginController(MemberDao memberDao) {
+        this.memberDao = memberDao;
     }
 
-    Member m = new Member();
-    m.setEmail(request.getParameter("email"));
-    m.setPassword(request.getParameter("password"));
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (request.getMethod().equals("GET")) {
+            return "/WEB-INF/jsp/auth/form.jsp";
+        }
 
-    if (request.getParameter("saveEmail") != null) {
-      Cookie cookie = new Cookie("email", m.getEmail());
-      response.addCookie(cookie);
-    } else {
-      Cookie cookie = new Cookie("email", "no");
-      cookie.setMaxAge(0);
-      response.addCookie(cookie);
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        if (request.getParameter("saveEmail") != null) {
+            Cookie cookie = new Cookie("email", email);
+            response.addCookie(cookie);
+        } else {
+            Cookie cookie = new Cookie("email", "no");
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
+
+        Member loginUser = memberDao.findByEmailAndPassword(email, password);
+        if (loginUser == null) {
+            request.setAttribute("refresh", "2;url=/app/auth/login");
+            throw new Exception("회원 정보가 일치하지 않습니다.");
+        }
+
+        request.getSession().setAttribute("loginUser", loginUser);
+        return "redirect:/";
     }
-
-    Member loginUser = memberDao.findByEmailAndPassword(m);
-    if (loginUser == null) {
-      request.setAttribute("refresh", "2;url=/app/auth/login");
-      throw new Exception("회원 정보가 일치하지 않습니다.");
-    }
-
-    request.getSession().setAttribute("loginUser", loginUser);
-    return "redirect:/";
-  }
 }
