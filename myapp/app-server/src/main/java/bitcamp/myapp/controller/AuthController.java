49b5321d738976ca21258145e1493de6deaa511a
shin2012649 <shin2012649@gmail.com>
2023-09-13 18:13:58 +0900

    update

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -5,6 +5,7 @@ import bitcamp.myapp.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,48 +18,49 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/auth")
 public class AuthController {
 
-    {
-        System.out.println("AuthController 생성됨!");
+  {
+    System.out.println("AuthController 생성됨!");
+  }
+
+  @Autowired
+  MemberService memberService;
+
+  @GetMapping("form")
+  public void form(@CookieValue(defaultValue = "") String email , Model model) {
+    model.addAttribute("email", email);
+  }
+
+  @PostMapping("login")
+  public String login(
+          String email,
+          String password,
+          String saveEmail,
+          HttpSession session,
+          Model model,
+          HttpServletResponse response) throws Exception {
+
+    if (saveEmail != null) {
+      Cookie cookie = new Cookie("email", email);
+      response.addCookie(cookie);
+    } else {
+      Cookie cookie = new Cookie("email", "no");
+      cookie.setMaxAge(0);
+      response.addCookie(cookie);
     }
 
-    @Autowired
-    MemberService memberService;
-
-    @GetMapping("form")
-    public void form() {
+    Member loginUser = memberService.get(email, password);
+    if (loginUser == null) {
+      model.addAttribute("refresh", "2;url=form");
+      throw new Exception("회원 정보가 일치하지 않습니다.");
     }
 
-    @PostMapping("login")
-    public String login(
-            String email,
-            String password,
-            String saveEmail,
-            HttpSession session,
-            Model model,
-            HttpServletResponse response) throws Exception {
+    session.setAttribute("loginUser", loginUser);
+    return "redirect:/";
+  }
 
-        if (saveEmail != null) {
-            Cookie cookie = new Cookie("email", email);
-            response.addCookie(cookie);
-        } else {
-            Cookie cookie = new Cookie("email", "no");
-            cookie.setMaxAge(0);
-            response.addCookie(cookie);
-        }
-
-        Member loginUser = memberService.get(email, password);
-        if (loginUser == null) {
-            model.addAttribute("refresh", "2;url=form");
-            throw new Exception("회원 정보가 일치하지 않습니다.");
-        }
-
-        session.setAttribute("loginUser", loginUser);
-        return "redirect:/";
-    }
-
-    @GetMapping("logout")
-    public String logout(HttpSession session) throws Exception {
-        session.invalidate();
-        return "redirect:/";
-    }
+  @GetMapping("logout")
+  public String logout(HttpSession session) throws Exception {
+    session.invalidate();
+    return "redirect:/";
+  }
 }
